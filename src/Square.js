82e4cc25ce5ec4b31b3fc64f247a45ca41eb5715
Litany <litany@Litanys-MacBook-Pro.local>
2022-09-24 14:57:04 +0100

    update grid scss with new colours

--- src/Square.js
+++ src/Square.js
@@ -8,9 +8,18 @@ export default function Square(props) {
     props.onSquareClick(props.id);
   }
 
+  // const squareClass = props.value === "X" ? "square_x" : ""
+
+  let squareClass = "";
+  if (props.value === "X") {
+    squareClass = "_square_x";
+  } else if (props.value === "O") {
+    squareClass = "_square_o";
+  }
+
   return (
     <div className="square" onClick={toggle}>
-      <p className="square__text">{props.value}</p>
+      <p className={`square__text${squareClass}`}>{props.value}</p>
     </div>
   );
 }
